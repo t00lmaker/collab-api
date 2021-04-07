cc06c868731b7275fe04cdcc01b50ea47c0f4a05
Luan Pontes <luanpontes2@gmail.com>
2021-04-06 22:08:30 -0300

    add reponse and errors classes

---
+++ src/presentation/helpers/response.js
@@ -0,0 +1,38 @@
+
+const { UnauthorizedError, ServerError } = require('../errors')
+
+module.exports = class Response {
+  static ok (body) {
+    return {
+      statusCode: 200,
+      body
+    }
+  }
+
+  static badRequest (error) {
+    return {
+      statusCode: 400,
+      body: {
+        error: error
+      }
+    }
+  }
+
+  static unauthorizedError () {
+    return {
+      statusCode: 401,
+      body: {
+        error: new UnauthorizedError().message
+      }
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500,
+      body: {
+        error: new ServerError().message
+      }
+    }
+  }
+}
