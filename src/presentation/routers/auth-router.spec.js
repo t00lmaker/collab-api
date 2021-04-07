733f15d2be0f21c172cc1fde8bf8362d08959ef6
Luan Pontes <luanpontes2@gmail.com>
2021-04-06 21:27:10 -0300

    add validation to 200 code

--- src/presentation/routers/auth-router.spec.js
+++ src/presentation/routers/auth-router.spec.js
@@ -1,7 +1,22 @@
+
 class AuthRouter {
+  constructor ({ authUseCase } = {}) {
+    this.authUseCase = authUseCase
+  }
+
   route (request) {
     const { email, secret } = request.body
     if (!email || !secret) {
+      if (this.authUseCase.auth(email, secret)) {
+        return {
+          statusCode: 200
+        }
+      } else {
+        return {
+          statusCode: 401
+        }
+      }
+    } else {
       return {
         statusCode: 400
       }
@@ -9,9 +24,33 @@ class AuthRouter {
   }
 }
 
+const createSut = () => {
+  const authUseCaseSpy = createAuthUseCase()
+  const sut = new AuthRouter({
+    authUseCase: authUseCaseSpy
+  })
+  return {
+    sut,
+    authUseCaseSpy
+  }
+}
+
+const createAuthUseCase = () => {
+  class AuthUseCaseSpy {
+    async auth (email, secret) {
+      this.email = email
+      this.secret = secret
+      return this.acessToken
+    }
+  }
+  const authUseCaseSpy = new AuthUseCaseSpy()
+  authUseCaseSpy.acessToken = 'valid_token'
+  return authUseCaseSpy
+}
+
 describe('AuthRouter', () => {
   it('should return 400 if no email is provider', () => {
-    const sut = new AuthRouter()
+    const { sut } = createSut()
     const request = {
       body: {
         secret: 'pass'
@@ -24,10 +63,10 @@ describe('AuthRouter', () => {
   })
 
   it('should return 400 if no secret is provider', () => {
-    const sut = new AuthRouter()
+    const { sut } = createSut()
     const request = {
       body: {
-        email: 'mail@mail.com'
+        email: 'valid@mail.com'
       }
     }
 
@@ -37,11 +76,25 @@ describe('AuthRouter', () => {
   })
 
   it('should return 400 if secret is empty', () => {
-    const sut = new AuthRouter()
+    const { sut } = createSut()
     const request = {
       body: {
         secret: '',
-        email: 'mail@mail.com'
+        email: 'valid@mail.com'
+      }
+    }
+
+    const response = sut.route(request)
+
+    expect(response.statusCode === 400)
+  })
+
+  it('should return 200 if valid credencial', () => {
+    const { sut } = createSut()
+    const request = {
+      body: {
+        secret: 'valid_pass',
+        email: 'valid@mail.com'
       }
     }
 
